acf1f5c9e10277f410cb33567e44d7ef192a0b34
KerimFortin <Kerimfortin@gmail.com>
2015-06-23 00:17:30 -0600

    borre esta parte por picaros

--- prueba.php
+++ prueba.php
@@ -1,11 +1,3 @@
 <?php 
     echo "JODANSE";
-    echo "tengo sueño";
-     echo "JODANSE";
-    echo "estoy bajando el programa hey que fresa esta esta onda";
-     echo "JODANSE";
-    echo "nuevo echo";
-     echo "JODANSE";
-    echo "nuevo echo";
-    echo "Hola chicos guapos";
-?>NO JODAN NI IDEA COMO SE SUBIO ESA IMAGEN ALLI QUE PEDO COMO SE SUBIO SE LOS JURO QUE YO NO LA SUBI
+?>
